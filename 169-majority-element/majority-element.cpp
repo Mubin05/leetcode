3413751e461a7904e8e9380ee8ae44fd2b699d39
Md Mubin <131579022+Mubin05@users.noreply.github.com>
2025-03-22 11:48:58 +0530

    Time: 0 ms (100.00%) | Memory: 27.9 MB (97.90%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,17 +1,43 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int,int> m;
+        // unordered_map<int,int> m;
+        // for(int i=0;i<nums.size();i++){
+        //     m[nums[i]]++;
+        // }
+        // int n=nums.size()/2;
+        // for(auto i:m){
+        //     if(i.second>n)
+        //     {
+        //         return i.first;
+        //     }
+        // }
+        // return -1;
+
+        int cnt=0,el;
         for(int i=0;i<nums.size();i++){
-            m[nums[i]]++;
-        }
-        int n=nums.size()/2;
-        for(auto i:m){
-            if(i.second>n)
-            {
-                return i.first;
+            if(cnt==0){
+                cnt=1;
+                el=nums[i];
+            }
+            else if(nums[i]==el){
+                cnt++;
+            }
+            else{
+                cnt--;
             }
         }
-        return -1;
+        int cnt1=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==el)cnt1++;
+        }
+
+        int n=nums.size()/2;
+        if(cnt1>n){
+            return el;
+        }
+        else{
+            return -1;
+        }
     }
 };
